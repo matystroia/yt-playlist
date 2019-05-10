bae20e88dac6e82c9919128578de6c40f6d2a07d
matt <stroiamatei@gmail.com>
2019-05-10 14:36:31 +0300

    video.js compatibility

--- content/video.js
+++ content/video.js
@@ -1,13 +1,26 @@
-let video = document.getElementsByClassName('video-stream html5-main-video')[0];
+function getYoutubeVersion() {
+    return 'modern';
+}
 
 function handleMessage(request, sender, sendResponse) {
+
     if (request.message === 'play') {
         video.click();
     } else if (request.message === 'getVideo') {
+        let videoId, videoTitle;
+
+        if (ytVersion === 'classic') {
+            videoId = document.getElementsByClassName('yt-uix-videoactionmenu')[0].getAttribute('data-video-id');
+            videoTitle = document.getElementsByClassName('ytp-title-link')[0].innerHTML;
+        } else if (ytVersion === 'modern') {
+            videoId = document.getElementsByTagName('ytd-watch-flexy')[0].getAttribute('video-id');
+            videoTitle = document.getElementsByClassName('ytp-title-link')[0].innerHTML;
+        }
+
         sendResponse({
             video: {
-                id: document.getElementsByClassName('yt-uix-videoactionmenu')[0].getAttribute('data-video-id'),
-                title: document.getElementsByClassName('ytp-title-link')[0].innerHTML
+                id: videoId,
+                title: videoTitle
             }
         })
     } else if (request.message === 'setVideo') {
@@ -15,11 +28,12 @@ function handleMessage(request, sender, sendResponse) {
     }
 }
 
+const ytVersion = getYoutubeVersion();
+
+browser.runtime.onMessage.addListener(handleMessage);
 
 // video.onended = function () {
 //     browser.runtime.sendMessage({message: 'getNext'}).then(response => {
 //         window.location.href = 'https://www.youtube.com/watch?v=' + response.video.id;
 //     });
 // };
-
-browser.runtime.onMessage.addListener(handleMessage);
